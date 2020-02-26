e0a30a82f2158fca97543c2cc57eb62b5b901f56
jallend1 <jallend1@gmail.com>
2020-02-25 17:20:51 -0800

    Initial commit and displays currently popular films

---
+++ src/components/elements/LoadMoreBtn.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const LoadMoreBtn = () => <div>Load More Button</div>
+export default LoadMoreBtn;
---
+++ src/components/elements/SearchBar.js
@@ -0,0 +1,3 @@
+import React from 'react';
+const SearchBar = () => <div>Search Bar</div>
+export default SearchBar;
