602726bf241b59a001fcb6b44b3cf6a750861d19
jallend1 <jallend1@gmail.com>
2020-02-26 10:19:45 -0800

    Add individual movie pages, search bar, info bar and actor information

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,5 +1,33 @@
 import React from 'react';
+import Navigation from './elements/Navigation';
+import MovieInfo from './elements/MovieInfo';
+import MovieInfoBar from './elements/MovieInfoBar';
+import Actor from './elements/Actor';
+import Grid from './elements/Grid';
+import Spinner from './elements/Spinner';
 
-const Movie = ({ movieId }) => <div>MOVIE: {movieId}</div>
+import { useMovieFetch } from './hooks/UseMovieFetch';
+
+const Movie = ({ movieId }) => {
+    const [movie, loading, error] = useMovieFetch(movieId);
+    if(error) return <div>Something bad happened</div>
+    if(loading) return <Spinner />
+    return (
+        <>
+            <Navigation movie={movie.original_title} />
+            <MovieInfo movie={movie}/>
+            <MovieInfoBar 
+                time={movie.runtime}
+                budget={movie.budget}
+                revenue={movie.revenue} 
+            />
+            <Grid header="Actors">
+                {movie.actors.map(actor => (
+                    <Actor key={actor.credit_id} actor={actor} />
+                ))}
+            </Grid>
+        </>
+    )
+}
 
 export default Movie;
